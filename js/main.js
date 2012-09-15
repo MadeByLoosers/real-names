4f30115b3e79c55b9950e569a73bcad611ef5539
John Lund <john@lundesign.co.uk>
2012-09-15 17:45:02 +0100

    First commit

---
+++ js/main.js
@@ -0,0 +1,72 @@
+var RN = RN || {};
+
+RN = (function () {
+
+	var settings = {
+		gInd : 0,
+		bInd : 0
+	};
+
+	function init(){
+
+		var s = settings;
+
+		// Get data from JSON and add to settings
+		$.getJSON('js/names.json', function (data) {
+
+			s.girls = data.girls;
+			s.boys = data.boys;
+
+			loadNames();
+
+		});
+
+		$('#more-names').on('click', function (e) {
+			e.preventDefault();
+
+			loadNames();
+		});
+
+	}
+
+	function loadNames() {
+		var s = settings;
+
+		// Get random names from settings list
+		var bRand = getRandomName(s.boys, s.bInd);
+			gRand = getRandomName(s.girls, s.gInd);
+
+		// Update HTML
+		var suggestions = $('#suggestions'),
+			bHTML = suggestions.find('.boys-name span'),
+			gHTML = suggestions.find('.girls-name span');
+
+		bHTML.html(s.boys[bRand]);
+		gHTML.html(s.girls[gRand]);
+
+		s.bInd = bRand;
+		s.gInd = gRand;
+	}
+
+	function getRandomName(nameArray, currentInd) {
+		var s = settings,
+			rnd = Math.floor((Math.random()*nameArray.length-1)+1);
+
+		if (rnd !== currentInd) {
+			return rnd;
+		}
+		else {
+			return getRandomName(nameArray, currentInd);
+		}
+		
+	}
+
+	return {
+		init: init
+	}
+
+})();
+
+$(document).ready(function () {
+	RN.init();
+});
